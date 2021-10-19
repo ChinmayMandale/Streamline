dc31bde46b8bbd950f1120537a220ead18687926
Sanjoli <sxs3872@mavs.uta.edu>
2021-10-18 20:01:57 -0500

    entity files
    Created user and admin entity files.

---
+++ server/streamline/src/main/java/com/uta/streamline/entities/Admin.java
@@ -0,0 +1,34 @@
+package com.uta.streamline.entities;
+
+import lombok.Data;
+
+@Data
+public class Admin {
+    private String adminName;
+    private String adminId;
+    private String password;
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ server/streamline/src/main/java/com/uta/streamline/entities/User.java
@@ -0,0 +1,53 @@
+package com.uta.streamline.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String userName;
+    private String userId;
+    private String ticketId;
+    private String password;
+    private String projectId;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+}
