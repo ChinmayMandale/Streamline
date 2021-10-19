2f44a0659fe41bdd7516070f8cf836efb600fa0e
Sanjoli <sxs3872@mavs.uta.edu>
2021-10-19 15:56:47 -0500

    User and admin class changes
    Removed getter and setter methods

--- server/streamline/src/main/java/com/uta/streamline/entities/User.java
+++ server/streamline/src/main/java/com/uta/streamline/entities/User.java
@@ -10,44 +10,4 @@ public class User {
     private String ticketId;
     private String password;
     private String projectId;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getTicketId() {
-        return ticketId;
-    }
-
-    public void setTicketId(String ticketId) {
-        this.ticketId = ticketId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(String projectId) {
-        this.projectId = projectId;
-    }
 }
