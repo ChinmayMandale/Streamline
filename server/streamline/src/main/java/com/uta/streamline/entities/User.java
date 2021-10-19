1a278eebcd48193e3e61d836509fe458b493a0ba
Sanjoli <sxs3872@mavs.uta.edu>
2021-10-19 16:43:18 -0500

    User and admin changes
    Added repository and service class for User and Admin

--- server/streamline/src/main/java/com/uta/streamline/entities/User.java
+++ server/streamline/src/main/java/com/uta/streamline/entities/User.java
@@ -1,13 +1,27 @@
 package com.uta.streamline.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
     private String userName;
-    private String userId;
     private String ticketId;
     private String password;
     private String projectId;
+    private Instant created;
+    private boolean enabled;
 }
