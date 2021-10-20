c987300c9c97dcbfb2d8af62e6c059b185c8c33b
Chinmay Mandale <chinmaymandale@gmail.com>
2021-10-19 19:00:50 -0500

    Client - authenticated login_signup

--- Client/streamline-angular/src/app/app.module.ts
+++ Client/streamline-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
