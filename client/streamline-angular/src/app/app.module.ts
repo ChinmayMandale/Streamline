7dea8fee333ef32683820266a7d797a970cdc370
Chinmay Mandale <chinmaymandale@gmail.com>
2021-10-25 13:39:19 -0500

    Client: Signup component working

--- client/streamline-angular/src/app/app.module.ts
+++ client/streamline-angular/src/app/app.module.ts
@@ -4,24 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {NgxWebstorageModule} from 'ngx-webstorage';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
+    AuthModule,
     NgxWebstorageModule.forRoot()
   ],
   providers: [],
