69da44a7a81b7c48aa9a7bc12e14cb4ea08aa8ce
Sanjoli <sxs3872@mavs.uta.edu>
2021-11-09 18:05:19 -0600

    changes to UserController
    changes to UserController

--- server/streamline/src/main/java/com/uta/streamline/controller/UserController.java
+++ server/streamline/src/main/java/com/uta/streamline/controller/UserController.java
@@ -4,13 +4,14 @@ import com.uta.streamline.entities.User;
 import com.uta.streamline.service.UserServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 @AllArgsConstructor
 public class UserController {
     private final UserServiceImpl userService;
@@ -18,5 +19,10 @@ public class UserController {
     @GetMapping("/getAllUsers")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/getByUsername/{username}")
+    public User getByUsername(@PathVariable String username) {
+        return userService.findByUsername(username);
     }
 }
