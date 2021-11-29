105ff97ce80dfaded53fd928ea1326dddf917576
Sanjoli <sxs3872@mavs.uta.edu>
2021-11-28 20:20:07 -0600

    updated create project html and ts
    updated create project html and ts file

--- client/streamline-angular/src/app/project/create-project/create-project.component.ts
+++ client/streamline-angular/src/app/project/create-project/create-project.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgSelectModule } from "@ng-select/ng-select";
+import { UserDTO } from 'src/app/shared/UserDTO';
+import { UserService } from 'src/app/services/user.service';
+import { ProjectService } from 'src/app/services/project.service';
+import { ProjectDTO } from 'src/app/shared/ProjectDTO';
 
 @Component({
   selector: 'app-create-project',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProjectComponent implements OnInit {
 
-  constructor() { }
+  users: Array<UserDTO>;
+  editProjectForm: FormGroup;
+  selected: any;
+  projectDTO: ProjectDTO;
+
+  constructor(private userService: UserService,
+    private projectService: ProjectService) { 
+      this.projectDTO = {
+        projectName: '',
+        users: [],
+        projectId: '',
+        tickets: []
+      };
+    }
 
   ngOnInit(): void {
+    this.editProjectForm = new FormGroup({
+      projectName: new FormControl('', Validators.required),
+      users: new FormControl('', Validators.required)
+    });
+    this.userService.getAllUsers().subscribe(res => {
+      this.users = res;
+    });
   }
+
+  submit() {
+    debugger
+    this.projectDTO.projectName = this.editProjectForm.value.projectName;
+    this.projectDTO.users = this.editProjectForm.value.users;
+
+    this.projectService.createProject(this.projectDTO).subscribe(res => {
+      debugger;
+      console.log(res);
+    })
+    console.log(this);
+  }
+
 
 }
