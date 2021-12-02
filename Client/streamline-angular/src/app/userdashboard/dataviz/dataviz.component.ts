6772323788618d30f6a7d46c6531c4d3de4cb32c
Chinmay Mandale <chinmaymandale@gmail.com>
2021-12-01 20:12:44 -0600

    client css minor changes

--- Client/streamline-angular/src/app/userdashboard/dataviz/dataviz.component.ts
+++ Client/streamline-angular/src/app/userdashboard/dataviz/dataviz.component.ts
@@ -23,39 +23,21 @@ export class DatavizComponent implements OnInit {
     {"status": "IN_PROGRESS", "value": ""},
     {"status": "TEST", "value": ""},
     {"status": "COMPLETE", "value": ""},
-  ]
+  ];
+
+  private userToTicketsMap: any = [
+    {"user": "", "tickets": ""}
+  ];
   
   private toDoTickets: Array<TicketDTO>;
   private inProgressTickets: Array<TicketDTO>;
   private testTickets: Array<TicketDTO>;
   private doneTickets: Array<TicketDTO>;
-  
-
-  // private margin = { top: 20, right: 20, bottom: 30, left: 50 };
-  // private width: number;
-  // private height: number;
-  // private x: any;
-  // private y: any;
-  // private svg: any;
-  // private line: d3Shape.Line<[number, number]>; // this is line defination
-  // private data: any[] = [
-  //   {date: new Date('2010-01-01'), value: 40},
-  //   {date: new Date('2010-01-04'), value: 93},
-  //   {date: new Date('2010-01-05'), value: 95},
-  //   {date: new Date('2010-01-06'), value: 130},
-  //   {date: new Date('2010-01-07'), value: 110},
-  //   {date: new Date('2010-01-08'), value: 120},
-  //   {date: new Date('2010-01-09'), value: 129},
-  //   {date: new Date('2010-01-10'), value: 107},
-  //   {date: new Date('2010-01-11'), value: 140},
-  // ];
 
   constructor(private userService: UserService,
               private authService: AuthService,
               private ticketService: TicketService,
               private dataVizService: DatavizService) {
-    // this.width = 960 - this.margin.left - this.margin.right;
-    // this.height = 500 - this.margin.top - this.margin.bottom;
   }
 
   ngOnInit(): void {
@@ -82,32 +64,5 @@ export class DatavizComponent implements OnInit {
 
   }
 
-
-  // private addXandYAxis() {
-  //   // range of data configuring
-  //   this.x = d3Scale.scaleTime().range([0, this.width]);
-  //   this.y = d3Scale.scaleLinear().range([this.height, 0]);
-  //   this.x.domain(d3Array.extent(this.data, (d) => d.date));
-  //   this.y.domain(d3Array.extent(this.data, (d) => d.value));
-  //   // Configure the X Axis
-  //   this.svg.append('g')
-  //     .attr('transform', 'translate(0,' + this.height + ')')
-  //     .call(d3Axis.axisBottom(this.x));
-  //   // Configure the Y Axis
-  //   this.svg.append('g')
-  //     .attr('class', 'axis axis--y')
-  //     .call(d3Axis.axisLeft(this.y));
-  // }
-
-  // private drawLineAndPath() {
-  //   this.line = d3Shape.line()
-  //     .x((d: any) => this.x(d.date))
-  //     .y((d: any) => this.y(d.value));
-  //   // Configuring line path
-  //   this.svg.append('path')
-  //     .datum(this.data)
-  //     .attr('class', 'line')
-  //     .attr('d', this.line);
-  // }
 
 }
